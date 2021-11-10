ed144a7aede2fb4bada22f31a80f1e37dd78e0ec
unknown <mark.avdiev@gmail.com>
2021-11-10 17:19:28 +0600

    the file changes and find add button

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_class_name('btn.btn-lg.btn-primary.btn-add-to-basket')
+    button = len(browser.find_element_by_class_name('btn.btn-lg.btn-primary.btn-add-to-basket').text)
+    assert button > 0, 'button is not found'
